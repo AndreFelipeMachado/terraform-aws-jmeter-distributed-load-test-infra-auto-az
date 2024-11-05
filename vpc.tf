cb93ccc3887cb19a0f1053ed0412d9b8a6447123
cb-sarathkumar <56917939+cb-sarathkumar@users.noreply.github.com>
2024-11-06 02:57:20 +0530

    updated the azs based on the input region

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,18 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  azs             = data.aws_availability_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   tags = {
